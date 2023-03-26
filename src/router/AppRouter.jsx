9c378a128788ddd047d76d9c61acd4a8e30d3426
kattae123 <kattae.kattae@gmail.com>
2023-03-26 16:19:50 -0400

    first commit

---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,23 @@
+import { Route, Routes } from 'react-router-dom'
+
+
+import LoginPage from '../auth/pages/LoginPage';
+import HeroesRoutes from '../heroes/routes/HeroesRoutes';
+
+const AppRouter = () => {
+    return (
+        <>
+
+            <Routes>
+
+                <Route path="login" element={<LoginPage />} />
+
+                <Route path="/*" element={<HeroesRoutes />} />
+
+
+            </Routes>
+        </>
+    )
+}
+
+export default AppRouter
